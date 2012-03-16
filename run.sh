38484a09b3a70ab756c4748f98df98dbb1b495e7
bgirard <bgirard@bulldozer.(none)>
2012-03-16 17:44:41 -0400

    Add sleep to run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 adb install -r bin/gl_measure-debug-unaligned.apk 1>&2
 adb shell am start -W -a org.mozilla.gl_measure  > /dev/null
+sleep 1
 adb shell cat /data/data/org.mozilla.gl_measure/files/output.txt
